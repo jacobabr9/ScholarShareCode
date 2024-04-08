950e2a60c90929eb0cc6fbfd62072a7621a61966
sachinaggerwal <sachinandcharlie@gmail.com>
2024-04-08 17:17:16 -0400

    adding about us page into django project

--- Django/django_project/scholar_share/urls.py
+++ Django/django_project/scholar_share/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('home/',views.home, name='scholar_share-home'),
     path('academic_integrity/',views.academic_integrity, name='scholar_share-academic_integrity'),
+    path('about_us/',views.about_us, name='scholar_share-about_us'),
     path('contact_us/',views.contact_us, name='scholar_share-contact_us'),
     path('civil_engineering/',views.civil_engineering, name='scholar_share-civil_engineering'),
     path('software_engineering/',views.software_engineering, name='scholar_share-software_engineering'),
